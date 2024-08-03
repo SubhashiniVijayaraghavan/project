2a6f01fec3de6e76c7340ca5e3330111e305e456
SubhashiniVijayaraghavan <727722euec183@skcet.ac.in>
2024-08-03 20:31:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const searchIcon=document.querySelector('.search-icon')
+const searchForm=document.querySelector('.search-form')
+searchIcon.addEventListener('click',()=>{
+searchForm.classList.toggle('active');
+cartItemsContainer.classList.remove('active');
+});
+
+
+const cartIcon=document.querySelector('.cart-icon')
+const cartItemsContainer=document.querySelector('.cart-items-container')
+cartIcon.addEventListener('click',()=>{
+cartItemsContainer.classList.toggle('active');
+ searchForm.classList.remove('active');
+});
+
+
